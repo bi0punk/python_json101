2c378476c3c1578f0c5f10fc2229f108ccf6cedf
sysbot <systecinfo_cl@criptext.com>
2022-01-25 00:17:58 -0300

    dbg

--- app2.py
+++ app2.py
@@ -11,47 +11,25 @@ from csv import writer
 url = "https://api-sismologia-chile.herokuapp.com/"
 df = pd.read_json(url)
 
+data = (df.iloc[0,0:7])
 
-
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    RED = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
-
-def sismos():
-    print(f"{bcolors.HEADER}ESTO ES UN TEXTO DE PRUEBA / este es un texto de prueba{bcolors.HEADER}")
-    print(f"{bcolors.OKBLUE}ESTO ES UN TEXTO DE PRUEBA / este es un texto de prueba{bcolors.OKBLUE}")
-    print(f"{bcolors.OKCYAN}ESTO ES UN TEXTO DE PRUEBA / este es un texto de prueba{bcolors.OKCYAN}")
-    print(f"{bcolors.OKGREEN}ESTO ES UN TEXTO DE PRUEBA / este es un texto de prueba{bcolors.OKGREEN}")
-    print(f"{bcolors.WARNING}ESTO ES UN TEXTO DE PRUEBA / este es un texto de prueba{bcolors.WARNING}")
-    print(f"{bcolors.RED}ESTO ES UN TEXTO DE PRUEBA / este es un texto de prueba{bcolors.RED}")
-    print(f"{bcolors.ENDC}ESTO ES UN TEXTO DE PRUEBA / este es un texto de prueba{bcolors.ENDC}")
-    print(f"{bcolors.BOLD}ESTO ES UN TEXTO DE PRUEBA / este es un texto de prueba{bcolors.BOLD}")
-    print(f"{bcolors.UNDERLINE}ESTO ES UN TEXTO DE PRUEBA / este es un texto de prueba{bcolors.UNDERLINE}")
-
-    data = (df.iloc[0,0:7])
-
+with open('datasismos.csv', 'a', newline='') as f_object:  
+    writer_object = writer(f_object)
+    writer_object.writerow(data)  
+    f_object.close()
+    df_notduplicate = pd.read_csv("datasismos.csv")
+    df_notduplicate.drop_duplicates(subset=None, keep='first', inplace=False, ignore_index=False)
     with open('datasismos.csv', 'a', newline='') as f_object:  
-        # Pass the CSV  file object to the writer() function
         writer_object = writer(f_object)
-        # Result - a writer object
-        # Pass the data in the list as an argument into the writerow() function
-        writer_object.writerow(data)  
-        # Close the file object
+        writer_object.writerow(df_notduplicate)  
         f_object.close()
 
 
-sismos()
+""" print(df_notduplicate) """ 
+    
+    
 
-
-
+    
 
 
 
---
+++ app3.py
@@ -0,0 +1,36 @@
+from prettytable import PrettyTable
+from urllib import parse, request
+import pandas as pd
+import requests
+import bs4
+import os
+from csv import writer
+
+
+cabezeras= []
+fuente_dat = ('http://www.sismologia.cl/ultimos_sismos.html')
+page = requests.get(fuente_dat)
+sopa = bs4.BeautifulSoup(page.text, 'lxml')
+tabla = sopa.find('table')
+
+for i in tabla.find_all('th'):
+        title = i.text.strip()
+        cabezeras.append(title)
+df = pd.DataFrame(columns = cabezeras )
+
+for filas in tabla.find_all('tr')[1:]:
+        info = filas.find_all('td')
+        filas_info = [td.text.strip() for td in info]
+        length = len(df)
+        df.loc[length] = filas_info
+data = (df.iloc[0,0:7])
+print(data)
+
+with open('sismos2.csv', 'a', newline='') as f_object:  
+        writer_object = writer(f_object)
+        writer_object.writerow(data)  
+        f_object.close()
+
+       
+
+
